9c8b10a135bceddf975b82fe752de37941160614
ZachNich <raiste.doctow@gmail.com>
2024-06-01 15:10:21 -0500

    add cli file reading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    path: std::path::PathBuf,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Cli::parse();
+    let content = std::fs::read_to_string(&args.path)?;
+    println!("file content: {}", content);
+    Ok(())
 }
